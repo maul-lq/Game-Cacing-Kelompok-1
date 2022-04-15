3e62c7583198826f2a9f18b74c8658ef4dae4818
MikoSky01 <38252750+MikoSky01@users.noreply.github.com>
2022-04-15 14:48:19 +0700

    build.bat update

--- Include/build.bat
+++ Include/build.bat
@@ -1,4 +1,10 @@
 @echo off
 @color 0f
 
-pyinstaller game.py -n "Game" -F -w -i "./res/ikon/ikon.ico" --clean
+@REM coba pakai ini jika terjadi error karena file tidak ditemukan!
+cd "Include"
+
+pyinstaller game.py -n "Game" -F -w -i "./res/ikon/ikon.ico" --clean --distpath "./bin/" --workpath "./bin/__temp__build/"
+rmdir /S  /Q ".\bin\__temp__build"
+rmdir /S  /Q ".\bin\res"
+xcopy ".\res" ".\bin\res" /E/H/I
