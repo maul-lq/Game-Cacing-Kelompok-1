0c15e13ba5f59c3948a1ffda29230009c75aae5f
MikoSky01 <38252750+MikoSky01@users.noreply.github.com>
2022-04-20 09:33:21 +0700

    skin cacing

--- Include/build.bat
+++ Include/build.bat
@@ -4,7 +4,7 @@
 @REM coba pakai ini jika terjadi error karena file tidak ditemukan!
 cd "Include"
 
-pyinstaller game.py -n "Game" -F -w -i "./res/ikon/ikon.ico" --clean --distpath "./bin/" --workpath "./bin/__temp__build/"
+pyinstaller game.py -n "Game Cacing" -F -w -i "./res/ikon/ikon.ico" --clean --distpath "./bin/" --workpath "./bin/__temp__build/"
 rmdir /S  /Q ".\bin\__temp__build"
 rmdir /S  /Q ".\bin\res"
 xcopy ".\res" ".\bin\res" /E/H/I
